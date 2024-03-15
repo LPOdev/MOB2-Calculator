b4d1928335fa8777f2717ed9c99f4731c849670d
lpodev <lpodev@proton.me>
2024-03-15 11:47:51 +0100

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Luís Pedro Pinheiro on 15.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
